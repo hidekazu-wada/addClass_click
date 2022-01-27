bc3fbadbf05fd777f125b15b0058f409ff94ddc2
hidekazu_wada <s.hazushi@gmail.com>
2022-01-27 22:30:37 +0900

    追加したクラスもトリガーに

--- js/my.js
+++ js/my.js
@@ -1,4 +1,12 @@
 // クリック
-jQuery(".trigger").on('click', function(){
-    jQuery(this).toggleClass('active');
-});
+jQuery( function() {
+	jQuery( document ).on( 'click', '.trigger', function() {
+		jQuery( this ).toggleClass('active');
+        jQuery('#main_menu').addClass('active');
+
+	} );
+    jQuery( document ).on( 'click', '.trigger.active', function() {
+		jQuery( this ).removeClass( 'active' );
+        jQuery('#main_menu').removeClass('active');
+	} );
+} );
